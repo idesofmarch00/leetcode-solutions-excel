1716836684874ba06242ddbdf22c0aabbc01349b
Sahil Ahmed <sahil.ahmed@fuelbuddy.in>
2024-07-03 09:51:15 +0530

    Added two sum solution

---
+++ 001_two_sum.js
@@ -0,0 +1,28 @@
+/*
+Metadata:
+questionNo: 1
+problemName: Two Sum
+problemStatement: Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+technique: Hash Map
+topic: Arrays
+difficulty: Easy
+*/
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (nums, target) {
+  const map = new Map();
+
+  for (let i = 0; i < nums.length; i++) {
+    const complement = target - nums[i];
+    if (map.has(complement)) {
+      return [map.get(complement), i];
+    }
+    map.set(nums[i], i);
+  }
+
+  return [];
+};
