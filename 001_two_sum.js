ded2ddd22f957029961b5f70e424942a879dbc6c
Sahil Ahmed <sa.idesofmarch@gmail.com>
2024-07-03 10:57:44 +0530

    Test prehook

--- 001_two_sum.js
+++ 001_two_sum.js
@@ -1,5 +1,6 @@
 /*
 Metadata:
+
 questionNo: 1
 problemName: Two Sum
 problemStatement: Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
